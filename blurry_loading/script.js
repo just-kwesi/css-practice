6d40dd6c3b9c07397f4f0a9f5f75a180246b643f
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-25 23:25:59 -0600

    js done for blur

---
+++ blurry_loading/script.js
@@ -0,0 +1,23 @@
+const progressText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+let int = setInterval(blurring, 30)
+function blurring() {
+  load++
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  progressText.innerText = `${load}%`
+  progressText.style.opacity = scale(load, 0, 100, 1, 0)
+
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+
+  //   console.log(scale(load, 0, 100, 1, 10))
+}
